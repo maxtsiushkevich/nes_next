e5d9e754f49105faa596aa786ad6c1cd50969b60
Max Tsiushkevich <max.tyushkevich@gmail.com>
2026-05-16 20:58:23 +0300

    Several instructions have been implemented. The flag register is now a single number.

--- processor/cpu/cpu.go
+++ processor/cpu/cpu.go
@@ -2,30 +2,43 @@ package processor
 
 import "fmt"
 
-type processorStatus struct {
-	carry             bool
-	zero              bool
-	interrupt_disable bool
-	decimal_mode      bool
-	break_command     bool
-	overflow          bool
-	negative          bool
-}
-
 type cpu struct {
-	pc  int16           // Program Counter
-	sp  int8            // Stack Pointer
-	a   int8            // Accumulator
-	irx int8            // Index Register X
-	iry int8            // Index Register Y
-	ps  processorStatus // Processor Status Register
+	pc  int16 // Program Counter
+	sp  byte  // Stack Pointer
+	a   byte  // Accumulator
+	irx byte  // Index Register X
+	iry byte  // Index Register Y
 
-	tick int8
+	ps byte // Processor Status Register
+	// Flags NV1B DIZC
+	// N - Negative
+	// V - Overflow
+	// 1 - always 1, not affected
+	// B - Break, not affected to CPU
+	// D - Decimal
+	// I - Interrupt Disable
+	// Z - Zero
+	// C - Carry
+	// Initial state - 0b00100100
+
+	tick byte
 }
+
+type CpuFlag int
+
+const (
+	Negative         CpuFlag = 7
+	Overflow         CpuFlag = 6
+	Break            CpuFlag = 4
+	Decimal          CpuFlag = 3
+	InterruptDisable CpuFlag = 2
+	Zero             CpuFlag = 1
+	Carry            CpuFlag = 0
+)
 
 func InitCpu() cpu {
 	return cpu{
-		ps: processorStatus{},
+		ps: 0b00100100, // Processor Status Register initial value
 	}
 }
 
@@ -34,5 +47,22 @@ func (cpu *cpu) Step(opcode byte) {
 }
 
 func PrintCpuState(cpu cpu) {
-	fmt.Printf("%+v", cpu)
+	fmt.Printf("%+v\n", cpu)
+	fmt.Printf("%#08b\n", cpu.ps)
+
+}
+
+func (cpu *cpu) GetFlag(flag CpuFlag) byte {
+	if cpu.ps&(1<<flag) != 0 {
+		return 1
+	}
+	return 0
+}
+
+func (cpu *cpu) SetFlag(state bool, flag CpuFlag) {
+	if state {
+		cpu.ps = cpu.ps | (1 << flag)
+	} else {
+		cpu.ps = cpu.ps &^ (1 << flag)
+	}
 }
--- processor/cpu/instructions.go
+++ processor/cpu/instructions.go
@@ -1,33 +1,68 @@
 package processor
 
+// Clear Carry
 func (cpu *cpu) Clc() {
-	cpu.ps.carry = false
+	cpu.SetFlag(false, Carry)
 }
 
+// Set Carry
 func (cpu *cpu) Sec() {
-	cpu.ps.carry = true
+	cpu.SetFlag(true, Carry)
 }
 
+// Clear Interrupt
 func (cpu *cpu) Cli() {
-	cpu.ps.carry = false
+	cpu.SetFlag(false, InterruptDisable)
 }
 
+// Set Interrupt
 func (cpu *cpu) Sei() {
-	cpu.ps.carry = true
+	cpu.SetFlag(true, InterruptDisable)
 }
 
+// Set Overflow
 func (cpu *cpu) Clv() {
-	cpu.ps.carry = false
+	cpu.SetFlag(true, Overflow)
 }
 
+// Clear Overflow
 func (cpu *cpu) Cld() {
-	cpu.ps.carry = false
+	cpu.SetFlag(false, Overflow)
 }
 
+// Set Decimal
 func (cpu *cpu) Sed() {
-	cpu.ps.carry = true
+	cpu.SetFlag(true, Decimal)
 }
 
-func (cpu *cpu) Adc(value byte) {
-	result := cpu.ps.carry + 
+// Add with Carry
+func (cpu *cpu) Adc(operand byte) {
+	result := uint16(cpu.GetFlag(Carry)) + uint16(operand) + uint16(cpu.a)
+
+	if result > 0xFF {
+		cpu.SetFlag(true, Carry)
+	}
+	if result == 0 {
+		cpu.SetFlag(true, Zero)
+	}
+	if (byte(result)^cpu.a)&(byte(result)^operand)&0x80 == 1 {
+		cpu.SetFlag(true, Overflow)
+	}
+	if byte(result)&(1<<7) == 0b10000000 {
+		cpu.SetFlag(true, Negative)
+	}
+
+	cpu.a = byte(result)
+
+}
+
+// Bitwise And
+func (cpu *cpu) And(operand byte) {
+	result := cpu.a & operand
+	if result == 0 {
+		cpu.SetFlag(true, Zero)
+	}
+	if result&(1<<7) == 0b10000000 {
+		cpu.SetFlag(true, Negative)
+	}
 }
